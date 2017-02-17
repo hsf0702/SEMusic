237da7cc979b0273e2b1b44e3399ca6aa0d69a15
neugaojin <894871108@qq.com>
2017-02-17 23:32:39 +0800

    2172

--- app/src/main/java/com/past/music/activity/MainActivity.java
+++ app/src/main/java/com/past/music/activity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.past.music.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,6 +17,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
+import android.widget.Toast;
 
 import com.jaeger.library.StatusBarUtil;
 import com.past.music.BaseActivity;
@@ -23,6 +29,9 @@ import com.past.music.service.PlayerService;
 import com.past.music.widget.CircleImageView;
 
 public class MainActivity extends BaseActivity {
+
+    //权限申请标志码
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0x01;
 
     private DrawerLayout mDrawerLayout;
     private Toolbar mToolbar;
@@ -44,6 +53,47 @@ public class MainActivity extends BaseActivity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        getMusicList();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        Animation operatingAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+        LinearInterpolator lin = new LinearInterpolator();
+        operatingAnim.setInterpolator(lin);
+        if (operatingAnim != null) {
+            mCircleImageView.startAnimation(operatingAnim);
+        }
+    }
+
+    //
+    private void getMusicList() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        } else {
+            setMusicList();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setMusicList();
+            } else {
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void setMusicList() {
         adapter = new MusicListAdapter(this);
         mRecycleView.setLayoutManager(new LinearLayoutManager(this));
         mRecycleView.setHasFixedSize(true);
@@ -59,18 +109,6 @@ public class MainActivity extends BaseActivity {
                 startService(intent);       //启动服务
             }
         });
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close);
-        mDrawerLayout.setDrawerListener(toggle);
-        toggle.syncState();
-
-        Animation operatingAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
-        LinearInterpolator lin = new LinearInterpolator();
-        operatingAnim.setInterpolator(lin);
-        if (operatingAnim != null) {
-            mCircleImageView.startAnimation(operatingAnim);
-        }
     }
 
     @Override
