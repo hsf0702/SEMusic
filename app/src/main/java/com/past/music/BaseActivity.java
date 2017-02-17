e5ce09b6d0d7c48c33954aea8dc4e66884f71bee
墨鸦 <moya@qufaya.com>
2017-02-17 22:10:05 +0800

    2171

--- app/src/main/java/com/past/music/BaseActivity.java
+++ app/src/main/java/com/past/music/BaseActivity.java
@@ -2,7 +2,10 @@ package com.past.music;/**
  * Created by gaojin on 2017/1/26.
  */
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jaeger.library.StatusBarUtil;
+import com.past.music.pastmusic.R;
 
 /**
  * =======================================================
@@ -13,5 +16,15 @@ import android.app.Activity;
  * 备注：
  * =======================================================
  */
-public class BaseActivity extends Activity {
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        setStatusBar();
+    }
+
+    protected void setStatusBar() {
+        StatusBarUtil.setColor(this, getResources().getColor(R.color.colorPrimary));
+    }
 }
