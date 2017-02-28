90aa7d803d81bc2dab55d55ec377d29d770aa387
neugaojin <894871108@qq.com>
2017-03-01 01:00:14 +0800

    GridView

--- app/src/main/java/com/past/music/fragment/MusicFragment.java
+++ app/src/main/java/com/past/music/fragment/MusicFragment.java
@@ -1,19 +1,24 @@
 package com.past.music.fragment;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.past.music.Config.BaseConfig;
+import com.past.music.adapter.GridViewAdapter;
 import com.past.music.api.Test;
 import com.past.music.pastmusic.R;
 import com.past.music.utils.FrescoImageLoader;
 import com.tsy.sdk.myokhttp.MyOkHttp;
 import com.tsy.sdk.myokhttp.response.GsonResponseHandler;
 import com.youth.banner.Banner;
+
+import org.reactivestreams.Subscription;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,14 +27,21 @@ import java.util.Map;
 
 
 public class MusicFragment extends Fragment {
+
+    private static final String TAG = "MusicFragment";
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private Subscription mSubscription;
     private String mParam1;
     private String mParam2;
 
-    private Banner banner = null;
+    private Banner banner;
+    private GridView mGridView;
     private List<String> images = new ArrayList<>();
+    private List<Test.ShowapiResBodyBean.PagebeanBean.SonglistBean> mData = new ArrayList<>();
+
+    private GridViewAdapter mGridViewAdapter;
 
     public MusicFragment() {
 
@@ -54,14 +66,14 @@ public class MusicFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_music, container, false);
         images.add("http://cimg2.163.com/catchimg/20090930/8458904_45.jpg");
         images.add("http://img1.imgtn.bdimg.com/it/u=2119707315,3199660736&fm=23&gp=0.jpg");
         images.add("http://img1.imgtn.bdimg.com/it/u=2504464883,3611462034&fm=23&gp=0.jpg");
         images.add("http://www.qqai.net/uploads/i_2_192535384x1019546146_21.jpg");
-
+        Log.i(TAG, "111" + String.valueOf(Looper.myLooper() == Looper.getMainLooper()));
         banner = (Banner) view.findViewById(R.id.banner);
         //设置图片加载器
         banner.setImageLoader(new FrescoImageLoader());
@@ -69,22 +81,27 @@ public class MusicFragment extends Fragment {
         banner.setImages(images);
         //banner设置方法全部调用完毕时最后调用
         banner.start();
+
+        mGridView = (GridView) view.findViewById(R.id.grid_view);
+        mGridViewAdapter = new GridViewAdapter(getContext(), mData);
+        mGridView.setAdapter(mGridViewAdapter);
+
         Map<String, String> params = new HashMap<>();
         params.put("showapi_appid", BaseConfig.APPID);
         params.put("showapi_sign", BaseConfig.SECRET);
-        params.put("topid", "3");
+        params.put("topid", "5");
         MyOkHttp.get().post(getContext(), BaseConfig.QQ_MUSIC_URL, params, new GsonResponseHandler<Test>() {
 
             @Override
             public void onFailure(int statusCode, String error_msg) {
-                Log.i("statusCode", statusCode + "");
-                Log.i("error_msg", error_msg);
             }
 
             @Override
             public void onSuccess(int statusCode, Test response) {
-                Log.i("statusCode", statusCode + "");
-                Log.i("response", response.getShowapi_res_body().getPagebean().getSonglist().get(0).getSongname());
+                for (int i = 0; i < 6; i++) {
+                    mData.add(response.getShowapi_res_body().getPagebean().getSonglist().get(i));
+                }
+                mGridViewAdapter.notifyDataSetChanged();
             }
         });
         return view;
@@ -102,5 +119,10 @@ public class MusicFragment extends Fragment {
         super.onStop();
         //结束轮播
         banner.stopAutoPlay();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/past/music/widget/IconView.java
@@ -0,0 +1,61 @@
+package com.past.music.widget;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.past.music.pastmusic.R;
+
+/**
+ * =======================================================
+ * 作者：gaojin
+ * 日期：2017/2/28 下午10:14
+ * 版本：
+ * 描述：
+ * 备注：
+ * =======================================================
+ */
+public class IconView extends RelativeLayout {
+
+    private Context mContext;
+    private SimpleDraweeView mHotPic;
+    private TextView mTvDes;
+
+    public IconView(Context context) {
+        this(context, null);
+    }
+
+    public IconView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public IconView(Context context, AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public IconView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        this.mContext = context;
+        initView();
+    }
+
+
+    private void initView() {
+        View.inflate(getContext(), R.layout.view_icon_view, this);
+        mHotPic = (SimpleDraweeView) findViewById(R.id.img_hot_pic);
+        mTvDes = (TextView) findViewById(R.id.tv_describe);
+    }
+
+    public void setmHotPic(String pic) {
+        Uri uri = Uri.parse(pic);
+        mHotPic.setImageURI(uri);
+    }
+
+    public void setmTvDes(String des) {
+        mTvDes.setText(des);
+    }
+}
