02b4f5d057dac7ca51aea6951ea335b919f1837c
墨鸦 <moya@qufaya.com>
2017-03-03 15:38:52 +0800

    音乐播放界面

--- app/src/main/java/com/past/music/activity/MainActivity.java
+++ app/src/main/java/com/past/music/activity/MainActivity.java
@@ -1,13 +1,16 @@
 package com.past.music.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
+import android.widget.RelativeLayout;
 
 import com.jaeger.library.StatusBarUtil;
 import com.past.music.BaseActivity;
@@ -15,17 +18,17 @@ import com.past.music.fragment.adapter.MainFragmentAdapter;
 import com.past.music.pastmusic.R;
 import com.past.music.widget.CircleImageView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     //权限申请标志码
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0x01;
 
     private DrawerLayout mDrawerLayout;
-
     private ViewPager mViewPager = null;
     private TabLayout mTabLayout = null;
     private MainFragmentAdapter mAdapter = null;
     private CircleImageView mCircleImageView = null;
+    private RelativeLayout mRlBottomPlay = null;
 
     private int mStatusBarColor;
     private int mAlpha = StatusBarUtil.DEFAULT_STATUS_BAR_ALPHA;
@@ -40,6 +43,8 @@ public class MainActivity extends BaseActivity {
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
         mTabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        mRlBottomPlay = (RelativeLayout) findViewById(R.id.rl_bottom);
+        initListener();
 
         FragmentManager fm = getSupportFragmentManager();
         //初始化自定义适配器
@@ -56,9 +61,24 @@ public class MainActivity extends BaseActivity {
         }
     }
 
+    private void initListener() {
+        mRlBottomPlay.setOnClickListener(this);
+    }
+
     @Override
     protected void setStatusBar() {
         mStatusBarColor = getResources().getColor(R.color.colorPrimary);
         StatusBarUtil.setColorForDrawerLayout(this, (DrawerLayout) findViewById(R.id.drawer_layout), mStatusBarColor, 0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.rl_bottom:
+                intent = new Intent(this, PlayMusicActivity.class);
+                startActivityByY(intent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/past/music/activity/PlayMusicActivity.java
+++ app/src/main/java/com/past/music/activity/PlayMusicActivity.java
@@ -1,16 +1,27 @@
 package com.past.music.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.jaeger.library.StatusBarUtil;
 import com.past.music.BaseActivity;
 import com.past.music.pastmusic.R;
 
 public class PlayMusicActivity extends BaseActivity {
 
+    private ImageView mImageBg = null;
+    private RelativeLayout mRlTotal = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_music);
+        mRlTotal = (RelativeLayout) findViewById(R.id.rl_total_layout);
     }
 
+    @Override
+    protected void setStatusBar() {
+        StatusBarUtil.setTranslucent(this);
+    }
 }
