37390b0d67297c091b0d07df8f36b2b101e097f4
neugaojin <894871108@qq.com>
2017-03-29 00:53:39 +0800

    recycleView 定位

--- app/src/main/java/com/past/music/dialog/MusicQueueFragment.java
+++ app/src/main/java/com/past/music/dialog/MusicQueueFragment.java
@@ -4,11 +4,10 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AlertDialog;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +15,6 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.past.music.entity.MusicEntity;
@@ -33,12 +31,11 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
- * Created by gaojin on 2017/3/25.
+ * Created by gaojin on 2017/3/28.
  */
 
-public class MusicListDialog extends AlertDialog {
+public class MusicQueueFragment extends DialogFragment {
 
-    private Context mContext;
 
     private ArrayList<MusicEntity> playlist = new ArrayList<>();
     private PlayListAdapter mAdapter = null;
@@ -46,6 +43,11 @@ public class MusicListDialog extends AlertDialog {
     private Handler mHandler;
     private int currentlyPlayingPosition = 0;
     private MusicEntity musicEntity = null;
+    private LinearLayoutManager layoutManager;
+    public Context mContext;
+    private boolean move = false;
+    private int index = 0;
+
 
     @BindView(R.id.recycle_view)
     RecyclerView mRecyclerView;
@@ -61,40 +63,58 @@ public class MusicListDialog extends AlertDialog {
         dismiss();
     }
 
+    public static MusicQueueFragment newInstance() {
+        MusicQueueFragment fragment = new MusicQueueFragment();
+        return fragment;
+    }
 
-    public MusicListDialog(@NonNull Context context) {
-        super(context, R.style.style_dialog);
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
         this.mContext = context;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dialog_play_list);
-        Window window = this.getWindow();
-        window.setGravity(Gravity.BOTTOM);
-        window.setWindowAnimations(R.style.bottom_dialog_in_style);
-        ButterKnife.bind(this);
-        mHandler = HandlerUtil.getInstance(mContext);
-        itemDecoration = new DividerItemDecoration(mContext.getResources(), R.color.text_gray, R.dimen.divider_1, DividerItemDecoration.VERTICAL_LIST);
-        mAdapter = new PlayListAdapter();
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.addItemDecoration(itemDecoration);
-        mRecyclerView.setAdapter(mAdapter);
-        new LoadSongs().execute();
+        //设置样式
+        setStyle(DialogFragment.STYLE_NO_FRAME, R.style.CustomDatePickerDialog);
     }
 
-
     @Override
-    public void show() {
-        super.show();
-        DisplayMetrics dm = new DisplayMetrics();
-        WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
-        wm.getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = (int) (dm.heightPixels * 0.6);
-        this.getWindow().setLayout(width, height);
+    public void onStart() {
+        super.onStart();
+        //设置fragment高度 、宽度
+        int dialogHeight = (int) (mContext.getResources().getDisplayMetrics().heightPixels * 0.6);
+        getDialog().getWindow().setLayout(WindowManager.LayoutParams.MATCH_PARENT, dialogHeight);
+        getDialog().setCanceledOnTouchOutside(true);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        //设置无标题
+        getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //设置从底部弹出
+        WindowManager.LayoutParams params = getDialog().getWindow().getAttributes();
+        params.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getDialog().getWindow().setAttributes(params);
+        View view = inflater.inflate(R.layout.dialog_play_list, container);
+        ButterKnife.bind(this, view);
+        mHandler = HandlerUtil.getInstance(mContext);
+        itemDecoration = new DividerItemDecoration(getActivity().getResources(), R.color.text_gray, R.dimen.divider_1, DividerItemDecoration.VERTICAL_LIST);
+        mAdapter = new PlayListAdapter();
+        layoutManager = new LinearLayoutManager(mContext);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.addItemDecoration(itemDecoration);
+        mRecyclerView.addOnScrollListener(new RecyclerViewListener());
+        mRecyclerView.setAdapter(mAdapter);
+        new LoadSongs().execute();
+        return view;
     }
 
     //异步加载recyclerview界面
@@ -115,8 +135,50 @@ public class MusicListDialog extends AlertDialog {
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            mAdapter.notifyDataSetChanged();
             textView_count.setText("（" + playlist.size() + "首）");
+            mAdapter.notifyDataSetChanged();
+            for (int i = 0; i < playlist.size(); i++) {
+                MusicEntity info = playlist.get(i);
+                if (info != null && MusicPlayer.getCurrentAudioId() == info.getSongId()) {
+                    moveToPosition(i);
+                }
+            }
+        }
+    }
+
+    private void moveToPosition(int n) {
+        index = n;
+        //先从RecyclerView的LayoutManager中获取第一项和最后一项的Position
+        int firstItem = layoutManager.findFirstVisibleItemPosition();
+        int lastItem = layoutManager.findLastVisibleItemPosition();
+        //然后区分情况
+        if (n <= firstItem) {
+            //当要置顶的项在当前显示的第一个项的前面时
+            mRecyclerView.scrollToPosition(n);
+        } else if (n <= lastItem) {
+            //当要置顶的项已经在屏幕上显示时
+            int top = mRecyclerView.getChildAt(n - firstItem).getTop();
+            mRecyclerView.scrollBy(0, top);
+        } else {
+            //当要置顶的项在当前显示的最后一项的后面时
+            mRecyclerView.scrollToPosition(n);
+            //这里这个变量是用在RecyclerView滚动监听里面的
+            move = true;
+        }
+    }
+
+    class RecyclerViewListener extends RecyclerView.OnScrollListener {
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+            if (move) {
+                move = false;
+                int n = index - layoutManager.findFirstVisibleItemPosition();
+                if (0 <= n && n < mRecyclerView.getChildCount()) {
+                    int top = mRecyclerView.getChildAt(n).getTop();
+                    mRecyclerView.scrollBy(0, top);
+                }
+            }
         }
     }
 
@@ -159,8 +221,6 @@ public class MusicListDialog extends AlertDialog {
             @BindView(R.id.img_dance)
             ImageView imageView;
 
-            @BindView(R.id.recycle_item)
-            LinearLayout mRecycleItem;
 
             public ItemViewHolder(View itemView) {
                 super(itemView);
