3ff11bdd7a4e5ace777b35e4b28bf7a1efe3a56d
neugaojin <894871108@qq.com>
2017-10-30 00:04:49 +0800

    主界面kotlin_Fragment替换

--- app/src/main/java/com/past/music/activity/MainActivity.java
+++ app/src/main/java/com/past/music/activity/MainActivity.java
@@ -1,24 +1,14 @@
 package com.past.music.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import com.jaeger.library.StatusBarUtil;
 import com.past.music.Config.BaseConfig;
-import com.past.music.fragment.adapter.MainFragmentAdapter;
+import com.past.music.fragment.AppMainFragment;
 import com.past.music.pastmusic.R;
-import com.past.music.utils.SysUtils;
 
 import butterknife.BindView;
-import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
@@ -29,50 +19,11 @@ public class MainActivity extends BaseActivity {
     @BindView(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
 
-    @BindView(R.id.view_pager)
-    ViewPager mViewPager;
-
-    @BindView(R.id.tab_layout)
-    TabLayout mTabLayout;
-
-    @BindView(R.id.img_start)
-    ImageView mImgStart;
-
-    @BindView(R.id.img_end)
-    ImageView mImgEnd;
-
-    @OnClick(R.id.img_end)
-    void search() {
-        Intent intent = new Intent(MainActivity.this, NetSearchWordsActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivityByX(intent, true);
-    }
-
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-
-
-    @OnClick(R.id.img_start)
-    void openDrawer() {
-        mDrawerLayout.openDrawer(Gravity.LEFT);
-    }
-
-    private MainFragmentAdapter mAdapter = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
-        FragmentManager fm = getSupportFragmentManager();
-        mAdapter = new MainFragmentAdapter(fm);
-        mViewPager.setAdapter(mAdapter);
-        mTabLayout.setupWithViewPager(mViewPager);
-        int statusBarHeight = SysUtils.getStatusBarHeight(this);
-
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) toolbar.getLayoutParams();
-        layoutParams.height += statusBarHeight;
-        toolbar.setLayoutParams(layoutParams);
+        getSupportFragmentManager().beginTransaction().add(R.id.content, AppMainFragment.Companion.newInstance()).commitAllowingStateLoss();
     }
 
     @Override
---
+++ app/src/main/java/com/past/music/fragment/AppMainFragment.kt
@@ -0,0 +1,48 @@
+package com.past.music.fragment
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.view.ViewPager
+import android.support.v7.widget.Toolbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.past.music.fragment.adapter.MainFragmentAdapter
+import com.past.music.pastmusic.R
+
+class AppMainFragment : Fragment() {
+
+    private var mToolBar: Toolbar? = null
+    private var mTabLayout: TabLayout? = null
+    private var mViewPager: ViewPager? = null
+    private var mAdapter: MainFragmentAdapter? = null
+
+    public override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                     savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        var view = inflater.inflate(R.layout.fragment_app_main, container, false)
+        mToolBar = view.findViewById(R.id.toolbar) as Toolbar
+        mTabLayout = view.findViewById(R.id.tab_layout) as TabLayout
+        mViewPager = view.findViewById(R.id.view_pager) as ViewPager
+        return view
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mTabLayout!!.tabGravity = TabLayout.GRAVITY_CENTER
+        val fm = activity.supportFragmentManager
+        mAdapter = MainFragmentAdapter(fm)
+        mViewPager!!.adapter = mAdapter
+        mTabLayout!!.setupWithViewPager(mViewPager)
+    }
+
+    companion object {
+        fun newInstance(): AppMainFragment {
+            val fragment = AppMainFragment()
+            val args = Bundle()
+            fragment.setArguments(args)
+            return fragment
+        }
+    }
+}// Required empty public constructor
