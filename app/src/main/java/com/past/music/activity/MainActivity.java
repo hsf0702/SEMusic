5356ea9464cfbb5700023b41cea76929e87c6190
neugaojin <894871108@qq.com>
2017-11-02 00:11:01 +0800

    动态权限整理

--- app/src/main/java/com/past/music/activity/MainActivity.java
+++ app/src/main/java/com/past/music/activity/MainActivity.java
@@ -1,9 +1,13 @@
 package com.past.music.activity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
+
 import com.past.music.fragment.AppMainFragment;
 import com.past.music.pastmusic.R;
+import com.past.music.utils.PermissionsChecker;
 
 import butterknife.BindView;
 
@@ -11,7 +15,14 @@ public class MainActivity extends BaseActivity {
 
     private static final String TAG = "MainActivity";
     //权限申请标志码
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0x01;
+    private static final int REQUEST_CODE = 0x00; // 请求码
+
+    private PermissionsChecker mPermissionsChecker; // 权限检测器
+
+    static final String[] PERMISSIONS = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+    };
+
 
     @BindView(R.id.drawer_layout)
     DrawerLayout mDrawerLayout;
@@ -21,6 +32,15 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportFragmentManager().beginTransaction().add(R.id.content, AppMainFragment.Companion.newInstance()).commitAllowingStateLoss();
+        mPermissionsChecker = new PermissionsChecker(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mPermissionsChecker.lacksPermissions(PERMISSIONS)) {
+            startPermissionsActivity();
+        }
     }
 
     @Override
@@ -29,7 +49,16 @@ public class MainActivity extends BaseActivity {
         this.overridePendingTransition(R.anim.push_up_in, R.anim.push_down_out);
     }
 
+    private void startPermissionsActivity() {
+        PermissionsActivity.Companion.startActivityForResult(this, REQUEST_CODE, PERMISSIONS);
+    }
+
     @Override
-    protected void setStatusBar() {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 拒绝时, 关闭页面, 缺少主要权限, 无法运行
+        if (requestCode == REQUEST_CODE && resultCode == PermissionsActivity.PERMISSIONS_DENIED) {
+            finish();
+        }
     }
 }
--- app/src/main/java/com/past/music/fragment/MineFragment.java
+++ app/src/main/java/com/past/music/fragment/MineFragment.java
@@ -1,23 +1,17 @@
 package com.past.music.fragment;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.past.music.MyApplication;
 import com.past.music.adapter.MyContentAdapter;
 import com.past.music.entity.SongListEntity;
 import com.past.music.event.CreateSongListEvent;
-import com.past.music.log.MyLog;
 import com.past.music.pastmusic.R;
 
 import org.greenrobot.eventbus.EventBus;
@@ -33,11 +27,6 @@ public class MineFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    //权限申请标志码
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0x01;
-
-    private String mParam1;
-    private String mParam2;
 
     @BindView(R.id.recycle_layout)
     RecyclerView mMusicList = null;
@@ -60,8 +49,6 @@ public class MineFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
         EventBus.getDefault().register(this);
     }
@@ -75,7 +62,6 @@ public class MineFragment extends Fragment {
         mMusicList.setLayoutManager(new LinearLayoutManager(getActivity()));
         mMusicList.setHasFixedSize(true);
         mMusicList.setAdapter(mAdapter);
-        getPermission();
         return view;
     }
 
@@ -85,31 +71,10 @@ public class MineFragment extends Fragment {
         EventBus.getDefault().unregister(this);
     }
 
-    private void getPermission() {
-        if (ContextCompat.checkSelfPermission(getActivity(),
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-        } else {
-
-        }
-    }
-
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onMessageEvent(CreateSongListEvent event) {
         List<SongListEntity> mList = MyApplication.songListDBService.query();
         mAdapter.setSongList(mList);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-            } else {
-                Toast.makeText(getActivity(), "Permission Denied", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 
     @Override
